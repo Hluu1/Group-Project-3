9ff847ed7d3614f1c6c52a4f3c8d847362f5f3ad
Laos27 <luzangelica.27@gmail.com>
2023-07-04 21:39:23 +0800

    population per state data added

--- Resources/schema.sql
+++ Resources/schema.sql
@@ -46,6 +46,50 @@ SELECT * FROM australia_sightings
 --Confirm Table was created correctly--
 SELECT * FROM total_sightings;
 
+--Create Table for Population Data per state--
+CREATE TABLE population_states
+(
+	Date DATE
+	, population_NSW INT
+	, population_VIC INT
+	, population_QLD INT
+	, population_SA INT
+	, population_WA INT
+	, population_TAS INT
+	, population_NT INT
+	, population_ACT INT
+);
+
+--Confirm Table was imported correctly--
+SELECT * FROM population_states;
+
+--Create a Table with Sightings per State per Year--
+CREATE TABLE sightings_state_year AS 
+WITH state_year AS
+(
+	SELECT 
+		"state"
+		, EXTRACT (YEAR FROM "date") AS "year"
+		, count("state") as "Total Sightings"
+	FROM cleaned_ufo_au
+	GROUP BY ("state", "year")
+	ORDER BY "year" ASC
+)
+SELECT * FROM state_year;
+
+CREATE TABLE population_states_year AS 
+WITH population_year AS
+	(
+	SELECT
+		*
+		, EXTRACT (YEAR FROM "date") AS "year"
+	FROM population_states
+	)
+
+
+	
+
+
 
 
 
