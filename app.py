b1f32a43b58afead43269c502e824a19d4d426a3
kirsty185 <126469546+kirsty185@users.noreply.github.com>
2023-07-06 20:45:23 +0800

    Add files via upload
    updated with print request to aide debugging

--- app.py
+++ app.py
@@ -4,22 +4,26 @@ app = Flask(__name__)
 #1. path to  index file
 @app.route('/')
 def index():
+    print("Server received request for 'index' page...")
     return render_template('index.html')
 
 #2. path to heatmap
 @app.route('/heatmap')
 def heatmap():
+    print("Server received request for 'heatmap' page...")
     return render_template('maps.html')
 
 #3. path to sighting over stat pop ratio
 @app.route('/ratiochart')
 def heatmap():
+    print("Server received request for 'ratiochart' page...")
     return render_template('ratio.html')
 
 
 #4. path to bar chart of state and shape of ufo
 @app.route('/shape_chart')
 def heatmap():
+    print("Server received request for 'shape_chart' page...")
     return render_template('shape_chart.html')
 
 if __name__ == '__main__':
