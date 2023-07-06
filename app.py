9dbd9290fc2bd83aa5ee8ac86f1475882cf0891e
kirsty185 <126469546+kirsty185@users.noreply.github.com>
2023-07-06 19:51:54 +0800

    Add files via upload
    update to app.py for bar chart path and name

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+#1. path to  index file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#2. path to heatmap
+@app.route('/heatmap')
+def heatmap():
+    return render_template('maps.html')
+
+#3. path to sighting over stat pop ratio
+@app.route('/ratiochart')
+def heatmap():
+    return render_template('ratio.html')
+
+
+#4. path to bar chart of state and shape of ufo
+@app.route('/shape_chart')
+def heatmap():
+    return render_template('shape_chart.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
