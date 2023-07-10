f65e07ef024d03c3a3cd5ad2dc05e47b24ffff5e
kirsty185 <126469546+kirsty185@users.noreply.github.com>
2023-07-10 14:42:27 +0800

    Add files via upload
    updated paths

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def heatmap():
     return render_template('maps.html')
 
 #3. path to sighting over stat pop ratio
-@app.route('/ratiochart')
+@app.route('/total_sightings_state_year')
 def heatmap():
-    print("Server received request for 'ratiochart' page...")
-    return render_template('ratio.html')
+    print("Server received request for 'total_sightings_state_year' page...")
+    return render_template('total_sightings_state_year.html')
 
 
 #4. path to bar chart of state and shape of ufo
@@ -25,6 +25,12 @@ def heatmap():
 def heatmap():
     print("Server received request for 'shape_chart' page...")
     return render_template('shape_chart.html')
+
+#5. path to line chart time of day (TOD) sightings
+@app.route('/shape_chart')
+def heatmap():
+    print("Server received request for 'TOD' page...")
+    return render_template('TOD.html')
 
 if __name__ == '__main__':
     app.debug = True
