0c6d44fb33c6d6245e175fdf3b028fcc3a82b062
Hluu1 <125692186+Hluu1@users.noreply.github.com>
2023-07-10 20:45:57 +0800

    Update heatmap.js
    I splitted the Javascript from the heatmap.html

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,81 +1,64 @@
-// heatmap.js
-
-// Create the map
 var myMap = L.map('map').setView([-27.47101, 153.024292], 6);
-
-var controlLayers = L.control.layers( null, null, {
-  position: "topright",
-  collapsed: false
-}).addTo(map);
 
 // Add the OpenStreetMap tile layer
 var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-controlLayers.addBaseLayer(light, 'Street Map');
 
-var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-  }).addTo(myMap);
-  controlLayers.addBaseLayer(terrain, 'Stamen Terrain basemap');
+// Create a marker cluster group
+var markers = L.markerClusterGroup();
 
-  fetch('Resources/ufo_coordinates.csv')
+// Create a heatmap layer
+var heat = L.heatLayer([], { radius: 25 });
+
+// Load UFO coordinates from ufo_coordinates.csv
+fetch('./Resources/ufo_coordinates.csv')
   .then(response => response.text())
   .then(csvString => {
-    // Parse CSV data using Papaparse
-    var data = Papa.parse(csvString, { header: true, dynamicTyping: true }).data;
+    // Parse CSV data using PapaParse
+    var data = Papa.parse(csvString, { header: true }).data;
 
-    // Create heatmap data array
-    var heatmapData = [];
-
-    // For each row in data, create a marker and add it to the map
-    // For each row, columns `Latitude` and `Longitude` are required
-    for (var i in data) {
-      var row = data[i];
-
-      // Validate latitude and longitude values
-      if (!isNaN(row.Latitude) && !isNaN(row.Longitude)) {
-        var lat = parseFloat(row.Latitude);
-        var lon = parseFloat(row.Longitude);
-        heatmapData.push([lat, lon]);
-
+    // For each row in data, create a marker and add it to the marker cluster group
+    // For each row, columns `latitude` and `longitude` are required
+    data.forEach(row => {
+      var lat = parseFloat(row.latitude);
+      var lon = parseFloat(row.longitude);
+      if (!isNaN(lat) && !isNaN(lon)) {
         var marker = L.marker([lat, lon], {
           opacity: 1
-        }).bindPopup(row.Title);
+        }).bindPopup(`Latitude: ${lat}<br>Longitude: ${lon}`);
 
-        marker.addTo(map);
+        markers.addLayer(marker);
+
+        heat.addLatLng([lat, lon]);
       } else {
-        console.error('Invalid latitude or longitude value at row', i + 2);
-        console.log('Row:', row);
+        console.warn('Invalid latitude or longitude value:', row);
       }
-    }
+    });
 
-    // Create heatmap layer if there are valid data points
-    if (heatmapData.length > 0) {
-      var heat = L.heatLayer(heatmapData).addTo(map);
-      controlLayers.addOverlay(heat, 'Heatmap');
+    // Add the marker cluster group and heatmap layer to the map
+    myMap.addLayer(markers);
+    myMap.addLayer(heat);
+  });
+
+// Toggle markers and heatmap based on checkbox state
+var toggleMarkers = document.getElementById('toggleMarkers');
+var toggleHeatmap = document.getElementById('toggleHeatmap');
+
+if (toggleMarkers && toggleHeatmap) {
+  toggleMarkers.addEventListener('change', function() {
+    if (toggleMarkers.checked) {
+      myMap.addLayer(markers);
+    } else {
+      myMap.removeLayer(markers);
     }
   });
 
-    // Toggle markers and heatmap based on checkbox state
-    var toggleMarkers = document.getElementById('toggleMarkers');
-    var toggleHeatmap = document.getElementById('toggleHeatmap');
-    toggleMarkers.addEventListener('change', function() {
-      if (toggleMarkers.checked) {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].addTo(myMap);
-        }
-      } else {
-        for (var i = 0; i < markers.length; i++) {
-          myMap.removeLayer(markers[i]);
-        }
-      }
-    });
-
-    toggleHeatmap.addEventListener('change', function() {
-      if (toggleHeatmap.checked) {
-        heat.addTo(myMap);
-      } else {
-        myMap.removeLayer(heat);
-      }
-    });
+  toggleHeatmap.addEventListener('change', function() {
+    if (toggleHeatmap.checked) {
+      myMap.addLayer(heat);
+    } else {
+      myMap.removeLayer(heat);
+    }
+  });
+}
